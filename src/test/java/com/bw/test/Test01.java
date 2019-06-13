f4a056e0f13203cbd3bf08d641cd1b756fe6fe15
cgh <email@cgh.com>
2019-06-13 08:27:13 +0700

    提交项目

---
+++ src/test/java/com/bw/test/Test01.java
@@ -0,0 +1,20 @@
+/**
+ * Create Date:2019年6月13日
+ */
+package com.bw.test;
+
+import org.junit.Test;
+
+/**
+ * <br>
+ * Title:TODO 类标题 <br>
+ * Description:TODO 类功能描述 <br>
+ * Author:陈广华(823865618@qq.com) <br>
+ * Date:2019年6月13日
+ */
+public class Test01 {
+	@Test
+	public void test02() {
+		System.out.println("holleWord");
+	}
+}
